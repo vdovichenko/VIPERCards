bcb7f1cf7cad1a81fcac12bb1b7a37ebb7895706
Dmytro Vdovychenko <dima.vdovichenko@gmail.com>
2017-10-27 15:42:00 +0300

    added needed pods

---
+++ Pods/Target Support Files/Pods-VIPERCards/Pods-VIPERCards-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VIPERCards : NSObject
+@end
+@implementation PodsDummy_Pods_VIPERCards
+@end
---
+++ VIPERCards/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  VIPERCards
+//
+//  Created by Dmytro Vdovychenko on 10/25/17.
+//  Copyright © 2017 Dmytro Vdovychenko. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
